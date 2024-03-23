b151f9d0c4d71bd3fe452ee5a1496b198b8686e6
Almish Subuktegin <almishsubuk@gmail.com>
2024-03-23 15:24:29 -0700

    added gitignore ostufdff

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAinTyXvVpAbGP4NFyAeU4sbSSDwGzZ3uM",
+  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
   authDomain: "react-chat-fda2d.firebaseapp.com",
   projectId: "react-chat-fda2d",
   storageBucket: "react-chat-fda2d.appspot.com",
   messagingSenderId: "458448311805",
   appId: "1:458448311805:web:c5bd6e35ceafbd07ebb594",
-  measurementId: "G-ER72VQ2ZWP"
+  measurementId: "G-ER72VQ2ZWP",
 };
 
 // Initialize Firebase
